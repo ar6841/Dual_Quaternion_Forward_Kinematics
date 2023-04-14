46268abbf244ccfe5e0380baa9c08a55075d7fe3
Arjun Raja <ar6841@nyu.edu>
2023-04-13 21:12:15 -0400

    Update constructors on DH params

--- DH_parameters.h
+++ DH_parameters.h
@@ -25,7 +25,7 @@ enum JointType{REVOLUTE,PRISMATIC,MIX};
             JointType type_i;
 
         public:
-            T theta_i,d_i; //Joint variables
+            T theta_i,d_i; //Joint variables (Don't make this private its too annoying)
 
             T theta_dot_i, d_dot_i; //joint velocity
 
@@ -36,7 +36,8 @@ enum JointType{REVOLUTE,PRISMATIC,MIX};
             DH_joint()
             {}
 
-            DH_joint(const T& theta, const T& alpha, const T& a, const T& d) : 
+            explicit DH_joint(const T& theta, const T& alpha, const T& a, const T& d) : 
+            
             theta_i(theta), alpha_i(alpha), a_i(a), d_i(d), 
             theta_dot_i(T(0)), d_dot_i(T(0)), 
             theta_2dot_i(T(0)), d_2dot_i(T(0)),
@@ -45,35 +46,65 @@ enum JointType{REVOLUTE,PRISMATIC,MIX};
                 //constructor to initialize the DH joint object
             }
 
-            DH_joint(const T& theta, const T& alpha, const T& a, const T& d, const T& theta_dot) : 
+            explicit DH_joint(const T& theta, const T& alpha, const T& a, const T& d, const JointType& jointType, const T& offset) : 
+            
             theta_i(theta), alpha_i(alpha), a_i(a), d_i(d), 
-            theta_dot_i(theta_dot), d_dot_i(T(0)), 
+            theta_dot_i(T(0)), d_dot_i(T(0)), 
             theta_2dot_i(T(0)), d_2dot_i(T(0)),
-            type_i(REVOLUTE), joint_offset(T(0))
+            type_i(REVOLUTE), joint_offset(T(offset))
             {
-
+                //constructor to initialize the DH joint object
             }
-
-            DH_joint(const T& theta, const T& alpha, const T& a, const T& d, const T& theta_dot, 
-            const T& d_dot) : 
-            theta_i(theta), alpha_i(alpha), a_i(a), d_i(d), 
-            theta_dot_i(theta_dot), d_dot_i(d_dot), 
-            theta_2dot_i(T(0)), d_2dot_i(T(0)),
-            type_i(MIX), joint_offset(T(0))
-            {
-                //constructor to initialize the DH joint object given velocities
-            }
-
-            DH_joint(const T& theta, const T& alpha, const T& a, const T& d, const T& theta_dot, 
-            const T& d_dot, const T& theta_2dot, const T& d_2dot) : 
-            theta_i(theta), alpha_i(alpha), a_i(a), d_i(d), 
-            theta_dot_i(theta_dot), d_dot_i(d_dot), 
-            theta_2dot_i(theta_2dot), d_2dot_i(d_2dot),
-            type_i(MIX), joint_offset(T(0))
-            {
-                //constructor to initialize the DH joint object given velocities and acc
-            }
+            // TODO: add more constructors
+            T getJointVar();
+            void setJointVar(const T& q);
             
     }; // class DH_joint
+
+    template<typename T>
+    inline T DH_joint<T>::getJointVar()
+    {
+        switch(type_i)
+        {
+            case REVOLUTE:
+                return theta_i - joint_offset;
+                break;
+
+            case PRISMATIC:
+                return d_i - joint_offset;
+                break;
+                
+            case MIX:
+                return NULL; //Add support for mixed joints
+            default:
+                return NULL;
+                break;
+            
+        }
+    
+    }
+    template<typename T>
+    inline void DH_joint<T>::setJointVar(const T& q)
+    {
+        switch(type_i)
+        {
+            case REVOLUTE:
+                theta_i = q+joint_offset;
+            break;
+
+            case PRISMATIC:
+                d_i = q + joint_offset;
+            break;
+
+            case MIX: //Add support for mixed joints
+            break;
+
+            default:
+            break;
+            
+        }
+    }
+
+
 
 } // namespace DH_joint
